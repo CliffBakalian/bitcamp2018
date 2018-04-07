169bc880e5935195fbc260d4926720fec579ec38
Corey Sackalosky <coreysackalosky@gmail.com>
2018-04-07 13:21:08 -0400

    Merge branch 'master' of https://github.com/CliffBakalian/bitcamp2018
    # Conflicts:
    #	.gradle/4.4/fileHashes/fileHashes.bin
    #	.gradle/4.4/fileHashes/fileHashes.lock
    #	.gradle/4.4/taskHistory/taskHistory.bin
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/edu/umcp/jacc/burnt/DataParser.java

--- app/src/main/java/edu/umcp/jacc/burnt/Informer.java
+++ app/src/main/java/edu/umcp/jacc/burnt/Informer.java
@@ -2,12 +2,15 @@ package edu.umcp.jacc.burnt;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class Informer extends AppCompatActivity {
+    private static final String TAG = "Informer";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,8 @@ public class Informer extends AppCompatActivity {
         Point p = new MapView(this).getLocationDisplay().getLocation().getPosition();
 
         String url = "https://api.darksky.net/forecast/d1e33e8fdffe9057c74d51f5a2de6e1d" + p.getY() + "," + p.getX();
+        Log.d(TAG, url);
+
         // call to UV
         NetworkManager.getInstance(this).makeRequest(this, Request.Method.GET, null, null, url, new CustomListener<String>() {
             @Override
