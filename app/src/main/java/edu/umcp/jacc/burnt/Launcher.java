621c48539cb3eb326b92d1baaf9edd233db419c7
= <=>
2018-04-06 21:45:13 -0400

    inital push

---
+++ app/src/main/java/edu/umcp/jacc/burnt/Launcher.java
@@ -0,0 +1,13 @@
+package edu.umcp.jacc.burnt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
