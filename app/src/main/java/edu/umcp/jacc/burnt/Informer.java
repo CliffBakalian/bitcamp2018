881f5e4941aac4a72a6e55fbb2421fa13a42b4a5
Justin Goodman <jugoodma@users.noreply.github.com>
2018-04-07 10:11:45 -0400

    Camera works yay

---
+++ app/src/main/java/edu/umcp/jacc/burnt/Informer.java
@@ -0,0 +1,12 @@
+package edu.umcp.jacc.burnt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Informer extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informer);
+    }
+}
--- app/src/main/java/edu/umcp/jacc/burnt/Launcher.java
+++ app/src/main/java/edu/umcp/jacc/burnt/Launcher.java
@@ -1,23 +1,11 @@
 package edu.umcp.jacc.burnt;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.ImageFormat;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.media.ImageReader;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Size;
 import android.view.SurfaceView;
-import android.view.TextureView;
-import android.view.View;
 import android.widget.Button;
 
 import java.util.concurrent.Semaphore;
@@ -36,16 +24,13 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-        Model.openCamera(getApplicationContext());
-        surfaceView = findViewById(R.id.cam_receiver);
 
-        btnCapture = findViewById(R.id.camera);
-        btnCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePicture();
-            }
-        });
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+
         /*
         Map<String,String> headers = new HashMap<>();
         headers.put("Content-Type", "application/x-www-form-urlencoded");
@@ -61,42 +46,5 @@ public class Launcher extends AppCompatActivity {
             }
         });
         */
-    }
-
-    private final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice cameraDevice) {
-            mCameraOpenCloseLock.release();
-            mCameraDevice = cameraDevice;
-            createCameraPreviewSession();
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
-            mCameraOpenCloseLock.release();
-            cameraDevice.close();
-            mCameraDevice = null;
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice cameraDevice, int error) {
-            mCameraOpenCloseLock.release();
-            cameraDevice.close();
-            mCameraDevice = null;
-        }
-    }
-    public void createCameraPreviewSession(){};
-    private void takePicture() {
-        if (cameraDevice == null)
-            return;
-        CameraManager manager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
-        try{
-            CameraCharacteristics cc = manager.getCameraCharacteristics(cameraDevice.getId());
-            Size[] picSize = null;
-            if (cc != null)
-                picSize = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
-        }catch (CameraAccessException e){
-
-        }
     }
 }
