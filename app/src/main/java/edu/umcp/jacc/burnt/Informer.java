80c967652b98338300a473334aada4a8e2a05532
Justin Goodman <jugoodma@users.noreply.github.com>
2018-04-07 13:17:59 -0400

    informer fix

--- app/src/main/java/edu/umcp/jacc/burnt/Informer.java
+++ app/src/main/java/edu/umcp/jacc/burnt/Informer.java
@@ -1,11 +1,11 @@
 package edu.umcp.jacc.burnt;
 
-import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.android.volley.Request;
-import com.esri.android.map.MapView;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class Informer extends AppCompatActivity {
     @Override
@@ -14,14 +14,14 @@ public class Informer extends AppCompatActivity {
         setContentView(R.layout.activity_informer);
 
         // get latitude and longitude data
-        Location l = new MapView(this).getLocationDisplayManager().getLocation();
+        Point p = new MapView(this).getLocationDisplay().getLocation().getPosition();
 
-        String url = "https://api.darksky.net/forecast/d1e33e8fdffe9057c74d51f5a2de6e1d" + l.getLatitude() + "," + l.getLongitude();
+        String url = "https://api.darksky.net/forecast/d1e33e8fdffe9057c74d51f5a2de6e1d" + p.getY() + "," + p.getX();
         // call to UV
         NetworkManager.getInstance(this).makeRequest(this, Request.Method.GET, null, null, url, new CustomListener<String>() {
             @Override
             public void getResult(String object) {
-                //DataParser.parseUV(object);
+                //new DataParser().parseUV(object);
             }
         });
     }
