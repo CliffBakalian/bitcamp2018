a1cba8ac2f9642a84a499e7a65dbc0009c2e5371
Justin Goodman <jugoodma@users.noreply.github.com>
2018-04-07 01:44:11 -0400

    fixing the camera

--- app/src/main/java/edu/umcp/jacc/burnt/Model.java
+++ app/src/main/java/edu/umcp/jacc/burnt/Model.java
@@ -1,50 +1,50 @@
 package edu.umcp.jacc.burnt;
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.*;
-import android.hardware.camera2.CameraCharacteristics;
-import android.support.annotation.NonNull;
 
-import static android.hardware.camera2.CameraCharacteristics.LENS_FACING;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.support.annotation.NonNull;
 
 public class Model {
     public void PicureStuff(Context c){
-        String[] lst;
+        String[] lst = new String[0];
         String id="BACK";
         CameraManager manager = (CameraManager) c.getSystemService(Context.CAMERA_SERVICE);
         try {
-            lst = manager.getCameraIdList();
-            for(int i = 0; i< lst.length; i++) {
-                CameraCharacteristics cc = manager.getCameraCharacteristics(lst[i]);
-                if (cc == null)
-                    throw new NullPointerException("No camera with id " + lst[i]);
-                if (cc.LENS_FACING.equals(CameraMetadata.LENS_FACING_FRONT))
-                    id = lst[i];
-            }
-            try{
-                manager.openCamera(id, new CameraDevice.StateCallback() {
-                    @Override
-                    public void onOpened(@NonNull CameraDevice camera) {
-
+            if (manager != null) {
+                for (String cameraId : manager.getCameraIdList()) {
+                    CameraCharacteristics cc = manager.getCameraCharacteristics(cameraId);
+                    Integer facing = cc.get(CameraCharacteristics.LENS_FACING);
+                    if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                        // found the front-facing camera!
+                        id = cameraId;
                     }
+                }
+                try {
+                    manager.openCamera(id, new CameraDevice.StateCallback() {
+                        @Override
+                        public void onOpened(@NonNull CameraDevice camera) {
 
-                    @Override
-                    public void onDisconnected(@NonNull CameraDevice camera) {
+                        }
 
-                    }
+                        @Override
+                        public void onDisconnected(@NonNull CameraDevice camera) {
 
-                    @Override
-                    public void onError(@NonNull CameraDevice camera, int error) {
+                        }
 
-                    }
-                }, null);
-            }
-            catch (SecurityException e){
-                lst = null;
+                        @Override
+                        public void onError(@NonNull CameraDevice camera, int error) {
+
+                        }
+                    }, null);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
             }
         } catch (CameraAccessException e) {
-            lst = null;
+            e.printStackTrace();
         }
     }
 
